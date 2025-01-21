5c62b2a24794e7520f9e9b8001fb36d71e3f302f
Aiperi <aiperi.khozhaeva@gmail.com>
2025-01-22 02:21:38 +0600

    Multiple changes with admin zone

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category
+admin.site.register(Category)
+
+from .models import Topping
+admin.site.register(Topping)
+
+
+from .models import Wrapper
+admin.site.register(Wrapper)
+
+from .models import IceCream
+# admin.site.register(IceCream)
+
+admin.site.empty_value_display = 'Не задано'
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',) 
+    list_filter = ('category',)
+    list_display_links = ('title',)
+
+    empty_value_display = 'Не задано'
+    filter_horizontal = ('toppings',)
+
+admin.site.register(IceCream, IceCreamAdmin)
+
+...
