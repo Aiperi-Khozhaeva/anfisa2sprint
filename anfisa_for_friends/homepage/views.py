dc2e27bcddceee55af1620b79436098954720f78
Aiperi <aiperi.khozhaeva@gmail.com>
2025-02-12 21:21:28 +0300

    Experimented with ordered by and limit

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,22 +1,4 @@
-# from django.shortcuts import render
-
-
-# from ice_cream.models import IceCream
-
-
-# def index(request):
-#     template_name = 'homepage/index.html'
-#     # ice_cream_list = IceCream.objects.all()
-#     ice_cream_list = IceCream.objects.values(
-#             'id', 'title', 'description'
-#         ).filter(is_on_main=True)
-#     context = {
-#         'ice_cream_list': ice_cream_list,
-#     }
-#     return render(request, template_name, context)
-
-
-# homepage/views.py
+from django.db.models import Q
 from django.shortcuts import render
 
 from ice_cream.models import IceCream
@@ -26,12 +8,33 @@ def index(request):
     # Заключаем вызов методов в скобки
     # (это стандартный способ переноса длинных строк в Python);
     # каждый вызов пишем с новой строки, так проще читать код:
+
     ice_cream_list = IceCream.objects.values(
             'id', 'title', 'description'
-        # Верни только те объекты, у которых в поле is_on_main указано True:
-        ).filter(is_on_main=True)
-    # .exclude(is_published=False)
+        ).filter(
+            is_published=True, is_on_main=True
+            # Q(is_published=True)
+            # & Q(is_on_main=True)
+            # | Q(title__contains='пломбир')
+            # & Q(is_published=True)
+            ).order_by('title')[1:4]
+
     context = {
         'ice_cream_list': ice_cream_list,
     }
+
     return render(request, template_name, context)
+
+
+"""========================================================================="""
+
+    # categories = Category.objects.values(
+    #     'id', 'output_order', 'title'
+    # ).order_by(
+    #     'output_order', 'title'
+    # )
+
+    # context = {
+    #     # 'ice_cream_list': ice_cream_list,
+    #     'categories': categories
+    # }
