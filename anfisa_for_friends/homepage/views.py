78910a14f2d2227f22533b8a79903056d44caee8
Aiperi <aiperi.khozhaeva@gmail.com>
2025-03-01 23:50:30 +0300

    New templates and migration

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -3,26 +3,50 @@ from django.shortcuts import render
 
 from ice_cream.models import IceCream
 
+# def index(request):
+#     template_name = 'homepage/index.html'
+#     # Заключаем вызов методов в скобки
+#     # (это стандартный способ переноса длинных строк в Python);
+#     # каждый вызов пишем с новой строки, так проще читать код:
+
+#     ice_cream_list = IceCream.objects.values(
+#             'id', 'title', 'description'
+#         ).filter(
+#             is_published=True, is_on_main=True
+#             # Q(is_published=True)
+#             # & Q(is_on_main=True)
+#             # | Q(title__contains='пломбир')
+#             # & Q(is_published=True)
+#             ).order_by('title')[1:4]
+
+#     context = {
+#         'ice_cream_list': ice_cream_list,
+#     }
+
+#     return render(request, template_name, context)
+
+
+"""============"""
+
+
+# homepage/views.py
+
+
 def index(request):
     template_name = 'homepage/index.html'
-    # Заключаем вызов методов в скобки
-    # (это стандартный способ переноса длинных строк в Python);
-    # каждый вызов пишем с новой строки, так проще читать код:
 
-    ice_cream_list = IceCream.objects.values(
-            'id', 'title', 'description'
-        ).filter(
-            is_published=True, is_on_main=True
-            # Q(is_published=True)
-            # & Q(is_on_main=True)
-            # | Q(title__contains='пломбир')
-            # & Q(is_published=True)
-            ).order_by('title')[1:4]
+    # ice_cream_list = IceCream.objects.all()
+    ice_cream_list = IceCream.objects.select_related(
+    'category'
+).filter(
+    # В точности то же самое:
+    category__is_published=True
+)    
+    # IceCream.objects.select_related('category')
 
     context = {
         'ice_cream_list': ice_cream_list,
     }
-
     return render(request, template_name, context)
 
 
